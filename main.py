7ffe735cffcd62a77f023d2ccfdfaea8143bcaf6
dm <dm@dmdeiMac.local>
2020-02-10 15:09:10 +0800

    neohost

--- main.py
+++ main.py
@@ -78,9 +78,25 @@ def clash(mu, fname):
       f.write(i)
       f.write("\r\n")
           
-        
+def neohost():
+   headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
+   myurl = "https://cdn.jsdelivr.net/gh/neoFelhz/neohosts@gh-pages/basic/hosts"
+   print(myurl)
+   req = urllib.request.Request(url=myurl, headers=headers)
+   txt = urllib.request.urlopen(req).read()
+   a = txt.decode('utf-8')
+   a.replace("\r\n", "\n")
+   a.replace("\r", "")
+   lines = a.split("\n")
+   fname = "./dist/"+"ad_host"+".conf"
+   f = open(fname, "x")
+   for i in lines:
+     if "0.0.0.0" in i:
+       f.write(i.replace("0.0.0.0", "DOMAIN").strip())
+       f.write("\r\n")
     
 Path("./dist").mkdir(parents=True, exist_ok=True)
 convert("6", "surge", takeSurge)
 convert("5", "qx", takeQX)
 clash("4", "clash")
+neohost()
