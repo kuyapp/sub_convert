37f3f31388162770da3353f3a74b578a6264c496
kuyapp <kuyapp@users.noreply.github.com>
2020-03-24 09:43:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,17 @@ from pathlib import Path
 def takeSurge(elem):
     if "Auto" in elem:
       return "0"
-    elem = elem.replace("通用", "5")
-    elem = elem.replace("京港", "g")
-    elem = elem.replace("沪港", "h")
-    elem = elem.replace("沪台", "i")
+
     elem = elem.replace("香港", "a")
     elem = elem.replace("新加坡", "b")
     elem = elem.replace("日本", "c")
     elem = elem.replace("台湾", "d")
     elem = elem.replace("韩国", "e")
     elem = elem.replace("美国", "f")
+    elem = elem.replace("京港", "g")
+    elem = elem.replace("沪港", "h")
+    elem = elem.replace("沪台", "i")
+    elem = elem.replace("通用", "j")
     return elem
 
 def takeQX(elem):
