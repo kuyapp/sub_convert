d7a8136bb2867abd3dc0deacff5ba683b87b64fc
kuyapp <kuyapp@users.noreply.github.com>
2020-09-29 11:06:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -154,7 +154,7 @@ def neohost():
 def fix():
     base_url = os.environ['SUB_URL']
     headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
-    myurl = base_url + "?surge=4";
+    myurl = base_url + "surge=4";
     print(myurl)
     req = urllib.request.Request(url=myurl, headers=headers)
     txt = urllib.request.urlopen(req).read()
